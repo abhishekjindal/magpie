66c76618ffe51bd0b3c4a7808d74485873aaa858
abhishekjindal <abhishekjindal93@gmail.com>
2017-04-12 23:24:07 -0400

    database update

--- models/userTable.js
+++ models/userTable.js
@@ -1,5 +1,5 @@
 const pg = require('pg');
-const connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/todo';
+const connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/magpie';
 const client = new pg.Client(connectionString);
 client.connect();
 const query = client.query(
