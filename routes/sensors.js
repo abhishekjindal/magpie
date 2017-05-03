ec4c217436be6052371d9f64b928db4c22bd8d9b
abhishekjindal <abhishekjindal93@gmail.com>
2017-05-03 03:27:36 -0400

    sensors update

--- routes/sensors.js
+++ routes/sensors.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const fs = require('fs');
+const pg = require('pg');
+const connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/magpie';
 const date = new Date;
 
-router.get('/receive',function(req, res){
-	res.send('listening');
-});
+client = {};
+pg.connect(connectionString, (err, thisclient, done) => {
+    client = thisclient;
+})
 
 router.post('/receive', function(request, respond) {
     var body = '';
@@ -15,13 +18,12 @@ router.post('/receive', function(request, respond) {
 
     
     request.on('data', function(data) {
-    	//var obj = JSON.parse(data);
         body += data;
     });
 
     request.on('end', function (){
 
-    	fs.writeFile('logfile2.csv', body, function() {
+    	fs.writeFile('activities_'+date+'.csv', body, function() {
             respond.end();
         });
     });
@@ -45,11 +47,8 @@ router.post('/dismissal', function(request, respond) {
     filePath = __dirname + '/sensors/';
     const results = [];
     var childname = '';
-    //console.log(request.files);
-
-    
+ 
     request.on('data', function(data) {
-        //var obj = JSON.parse(data);
         body += data;
         console.log(body);
         parameters = body.split('&');
@@ -57,50 +56,29 @@ router.post('/dismissal', function(request, respond) {
         date = (parameters[0].split('='))[1];
         time = (parameters[2].split('='))[1];
         id = (parameters[1].split('='))[1];
-        //id_int = parseInt(id,10);
-          pg.connect(connectionString, (err, client, done) => {
-            // Handle connection errors
-            if(err) {
-              done();
-              console.log(err);
-              return res.status(500).json({success: false, data: err});
-            }
-    
-            const query = client.query('SELECT child_id FROM sensors WHERE node_id=($1)',[id]);
-            child_id_int = -1;
-            query.on('row', (row) => {
-               child_id_int = parseInt(row.child_id,10);
-            });
-            query.on('end', () => {
-                done();
-                const query1 = client.query('SELECT * FROM children WHERE child_id=($1)',[child_id_int]);
+        const query = client.query('SELECT child_id FROM sensors WHERE node_id=($1)',[id]);
+        child_id_int = -1;
+        query.on('row', (row) => {
+           child_id_int = parseInt(row.child_id,10);
+        });
+        query.on('end', () => {
+            const query1 = client.query('SELECT * FROM children WHERE child_id=($1)',[child_id_int]);
 
-                query1.on('row', (row) => {
-                    childname = row.name;
-                    results.push(row);
-                    console.log(childname);
-
-                });
-                query1.on('end', () => { 
-                  done();
-                  const query2 = client.query('INSERT INTO dismissal(name) VALUES($1);',[childname])
-                });
+            query1.on('row', (row) => {
+                childname = row.name;
+                results.push(row);
+                console.log(childname);
 
             });
-            
-
-
-          });
-
-
+            query1.on('end', () => { 
+              const query2 = client.query('INSERT INTO dismissal(name) VALUES($1);',[childname])
+            });
+        });
     });
     
     request.on('end', function (){
-        //console.log(body);
         respond.send("received");
     });
-
-    
 });
 
 
